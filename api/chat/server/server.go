56c7546b5f67ffb5f44f084f352f3b19f7a1fe3b
Thiago Ripardo <tmripardo@gmail.com>
2024-04-12 04:34:38 -0300

    feat: updating the code with many bacons

--- api/chat/server/server.go
+++ api/chat/server/server.go
@@ -1,10 +1,14 @@
 package server
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
 	"sync"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"github.com/thiagoripardo/k3s-example/api/chat/domain/model"
 )
 
 type Server struct {
@@ -27,8 +31,24 @@ func New() *Server {
 }
 
 func (s *Server) Run() error {
+	instanceID := uuid.New()
+
 	s.srv.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "echo v4 server")
+		return c.String(http.StatusOK, "Welcome to lovelybacon.com!")
+	})
+
+	s.srv.GET("/generator", func(c echo.Context) error {
+		baconRes := model.BaconResponse{
+			InstanceID: instanceID.String(),
+		}
+
+		randomInt := rand.Intn(50)
+		bacon := model.Bacons[randomInt%len(model.Bacons)]
+		baconRes.Bacon = bacon
+
+		fmt.Printf("instance: %s generated a bacon\n", baconRes.InstanceID)
+
+		return c.JSON(http.StatusOK, baconRes)
 	})
 
 	s.srv.GET("/health", func(c echo.Context) error {
